dafdd5f02e56e16012ab2ad4f1b7f322e0dc2af7
idris10215 <mohammedidris10215@gmail.com>
2025-07-15 22:57:08 +0530

    Updated the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import Search from "./components/Search";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <>
-      
-    </>
-  )
+    <main>
+      <div className="pattern" />
+
+      <div className="wrapper">
+        <header>
+          <img src="./hero.png" alt="Hero Banner" />
+          <h1>Find <span className="text-gradient">Movies</span> That You'll Enjoy Without The Hastle</h1>
+        </header>
+
+        <Search />
+      </div>
+    </main>
+  );
 }
 
-export default App
+export default App;
